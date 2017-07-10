2ae5b233612b87ff46802ab3a10fbdecafa02945
alirezah320 <alirezah320@yahoo.com>
2017-07-11 01:07:35 +0430

    enterSelecton added

--- print_listener.py
+++ print_listener.py
@@ -1,14 +1,16 @@
 from pccListener import pccListener
 from error_listener import NotDeclaredException
 from pccLexer import pccLexer
+from pccParser import pccParser
 
 
 class pccPrintListener(pccListener):
     def __init__(self, name):
         super(pccPrintListener, self).__init__()
-        self.code = "" # bytecode
+        self.code = ""  # bytecode
         self.name = name
         self.block_number = 0
+        self.label_number = 0
         self.variable_number = 1
         self.variable_table = {}
         self.var_type_table = {
@@ -33,7 +35,6 @@ class pccPrintListener(pccListener):
             'char': 'bipush'
         }
 
-
     def newVar(self, name, vtype):
         size = 2 if vtype in ('long', 'double') else 1
         self.variable_table[(name, self.block_number)] = (self.variable_number, vtype)
@@ -49,36 +50,32 @@ class pccPrintListener(pccListener):
                 raise NotDeclaredException(name)
         return self.variable_table[(name, block)]
 
-
     def getBytecode(self):
         return self.code
-
 
     def _add_ins(self, instruction, var_type=None):
         v = self.var_type_table[var_type] if var_type else ''
         self.code += v + instruction + '\n'
 
-
     def _push_constant(self, constant, var_type):
         constant = str(constant)
         if var_type == 'float' or var_type == 'double':
-                constant += '.' if not '.' in constant else ''
+            constant += '.' if not '.' in constant else ''
         elif var_type == 'char':
             constant = ord(constant.replace("'", ""))
 
         self._add_ins("%s %s" % (self.constant_table[var_type], constant))
 
-
-
     def enterCompoundStatement(self, ctx):
         self.block_number += 1
+        if ctx.getChildCount() == 3:
+            self.enterBlockItemList(ctx.getChild(1))
 
     def exitCompoundStatement(self, ctx):
         for name, block_number in self.variable_table.keys():
             if block_number == self.block_number:
                 del self.variable_table[(name, block_number)]
         self.block_number -= 1
-
 
     def println(self, var_id, var_type, arr_index=None):
         self._add_ins("getstatic java/lang/System/out Ljava/io/PrintStream;")
@@ -92,7 +89,6 @@ class pccPrintListener(pccListener):
 
         self._add_ins("invokevirtual java/io/PrintStream/println(%s)V" % self.print_var_type_table[var_type])
 
-
     def enterProgram(self, ctx):
         self._add_ins('.class public %s' % self.name)
         self._add_ins('.super java/lang/Object')
@@ -104,14 +100,12 @@ class pccPrintListener(pccListener):
         self._add_ins('.end method')
 
         self._add_ins('.method public static main([Ljava/lang/String;)V')
-        self._add_ins('.limit stack 10000') # hardcoded!
-        self._add_ins('.limit locals 10000') # hardcoded!
-
+        self._add_ins('.limit stack 10000')  # hardcoded!
+        self._add_ins('.limit locals 10000')  # hardcoded!
 
     def exitProgram(self, ctx):
         self._add_ins("return")
         self._add_ins(".end method")
-
 
     def enterDeclaration(self, ctx):
         var_type, ids, _ = list(ctx.getChildren())
@@ -124,8 +118,8 @@ class pccPrintListener(pccListener):
             c = self.getLastChild(child)
             c_count = c.getChildCount()
 
-            if c_count == 0 or c_count == 3: # var
-                var_id = self.newVar((c.getChild(0) if c_count == 3 else c).getText(), var_type) # TODO: raise exception if variable exists
+            if c_count == 0 or c_count == 3:  # var
+                var_id = self.newVar((c.getChild(0) if c_count == 3 else c).getText(), var_type)  # TODO: raise exception if variable exists
                 self._push_constant(0, var_type)
                 self._add_ins("store %s" % var_id, var_type)
 
@@ -136,20 +130,19 @@ class pccPrintListener(pccListener):
                 self._add_ins("newarray %s" % var_type)
                 self._add_ins("astore %s" % var_id)
 
-            if c_count == 3: # declration with assignment
+            if c_count == 3:  # declration with assignment
                 self.enterAssignmentExpression(c)
-
 
     def enterAssignmentExpression(self, ctx):
         if ctx.getChildCount() == 3:
             assignee, _, value = list(ctx.getChildren())
             assignee = self.getLastChild(assignee)
-            if assignee.getChildCount() == 0: # identifier
+            if assignee.getChildCount() == 0:  # identifier
                 var_id, var_type = self.getVar(assignee.getText(), self.block_number)
                 self.calculateExpression(value, var_type)
                 self._add_ins("store %s" % var_id, var_type)
 
-            elif assignee.getChildCount() == 4: # array
+            elif assignee.getChildCount() == 4:  # array
                 var_id, var_type = self.getVar(assignee.getChild(0).getText(), self.block_number)
                 index = assignee.getChild(2)
                 self._add_ins('aload %s' % var_id)
@@ -157,23 +150,22 @@ class pccPrintListener(pccListener):
                 self.calculateExpression(value, var_type)
                 self._add_ins('astore', var_type)
 
-            elif assignee.getChildCount() == 3: # struct
+            elif assignee.getChildCount() == 3:  # struct
                 pass
-
 
     def calculateExpression(self, ctx, var_type):
         ctx = self.getLastChild(ctx)
 
-        if ctx.getChildCount() == 0: # identifier or constant value
-            if ctx.getSymbol().type == pccLexer.Identifier: # identifier
+        if ctx.getChildCount() == 0:  # identifier or constant value
+            if ctx.getSymbol().type == pccLexer.Identifier:  # identifier
                 var_id, vt = self.getVar(ctx.getText(), self.block_number)
                 if vt != var_type:
                     raise TypeError("wrong type '%s'" % ctx.getText())
                 self._add_ins("load %s" % var_id, var_type)
-            else: # constant value
+            else:  # constant value
                 self._push_constant(ctx.getText(), var_type)
 
-        if ctx.getChildCount() == 4: # array value
+        if ctx.getChildCount() == 4:  # array value
             var_id, vt = self.getVar(ctx.getChild(0).getText(), self.block_number)
             if vt != var_type:
                 raise TypeError("wrong type '%s'" % ctx.getText())
@@ -182,7 +174,7 @@ class pccPrintListener(pccListener):
             self.calculateExpression(arr_index, 'int')
             self._add_ins('aload', var_type)
 
-        elif ctx.getChildCount() == 3: # expr
+        elif ctx.getChildCount() == 3:  # expr
             if ctx.getChild(0).getText() == '(':
                 self.calculateExpression(ctx.getChild(1), var_type)
             else:
@@ -200,9 +192,37 @@ class pccPrintListener(pccListener):
                 elif o == '/':
                     self._add_ins("div", var_type)
 
-
-
     def getLastChild(self, ctx):
         if ctx.getChildCount() == 0 or ctx.getChildCount() > 1:
             return ctx
         return self.getLastChild(ctx.getChild(0))
+
+    def newLabel(self):
+        self.label_number += 1
+        return "label_%s" % self.label_number
+
+    def enterStatement(self, ctx):
+        statement_type = ctx.getRuleIndex()
+
+        if statement_type == pccParser.RULE_selectionStatement:
+            self.enterSelectionStatement(ctx)
+
+        elif statement_type == pccParser.RULE_iterationStatement:
+            self.enterIterationStatement(ctx)
+
+        elif statement_type == pccParser.RULE_labeledStatement:
+            self.enterLabeledStatement(ctx)
+
+    def enterSelectionStatement(self, ctx):
+        selection_type = ctx.getChild(0).getSymbol().type
+
+        if selection_type == pccLexer.If:
+            self.calculateExpression(ctx.getChild(2), 'int')
+            label = self.newLabel()
+            self._add_ins("ifeq %s" % label)
+            self.enterStatement(ctx.getChild(4))
+            self._add_ins("%s:" % label)
+
+        elif selection_type == pccLexer.Switch:
+            pass
+
