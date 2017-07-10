82c0cfb2d4d9d02d84e6499e8558121960f3f842
AminHP <mdan.hagh@gmail.com>
2017-07-10 19:06:01 +0430

    add type detection

--- print_listener.py
+++ print_listener.py
@@ -9,14 +9,36 @@ class pccPrintListener(pccListener):
         self.code = "" # bytecode
         self.name = name
         self.block_number = 0
+        self.variable_number = 1
         self.variable_table = {}
+        self.var_type_table = {
+            'int': 'i',
+            'float': 'f',
+            'double': 'd',
+            'long': 'l',
+            'char': 'i'
+        }
+        self.print_var_type_table = {
+            'int': 'I',
+            'float': 'F',
+            'double': 'D',
+            'long': 'J',
+            'char': 'C'
+        }
+        self.constant_table = {
+            'int': 'ldc',
+            'float': 'ldc',
+            'double': 'ldc2_w',
+            'long': 'ldc2_w',
+            'char': 'bipush'
+        }
 
 
-    def newVar(self, name):
-        new_id = len(self.variable_table) + 1 
-        self.variable_table[(name, self.block_number)] = new_id
-        return new_id
-
+    def newVar(self, name, vtype):
+        size = 2 if vtype in ('long', 'double') else 1
+        self.variable_table[(name, self.block_number)] = (self.variable_number, vtype)
+        self.variable_number += size
+        return self.variable_number - size
 
     def getVar(self, name, block):
         while True:
@@ -32,32 +54,43 @@ class pccPrintListener(pccListener):
         return self.code
 
 
-    def _add_ins(self, instruction):
-        self.code += instruction + '\n'
+    def _add_ins(self, instruction, var_type=None):
+        v = self.var_type_table[var_type] if var_type else ''
+        self.code += v + instruction + '\n'
+
+
+    def _push_constant(self, constant, var_type):
+        constant = str(constant)
+        if var_type == 'float' or var_type == 'double':
+                constant += '.' if not '.' in constant else ''
+        elif var_type == 'char':
+            constant = ord(constant.replace("'", ""))
+
+        self._add_ins("%s %s" % (self.constant_table[var_type], constant))
 
 
 
     def enterCompoundStatement(self, ctx):
-        self.block_number +=1
+        self.block_number += 1
 
     def exitCompoundStatement(self, ctx):
-        self.block_number -=1
+        for name, block_number in self.variable_table.keys():
+            if block_number == self.block_number:
+                del self.variable_table[(name, block_number)]
+        self.block_number -= 1
 
 
     def println(self, var_id, var_type, arr_index=None):
-        if var_type == 'int':
-            var_type = 'I'
-
         self._add_ins("getstatic java/lang/System/out Ljava/io/PrintStream;")
 
         if arr_index:
             self._add_ins('aload %s' % var_id)
-            self._add_ins('bipush %s' % arr_index)
-            self._add_ins('iaload')
+            self._add_ins('ldc %s' % arr_index)
+            self._add_ins('aload', var_type)
         else:
-            self._add_ins("iload %s" % var_id)
+            self._add_ins("load %s" % var_id, var_type)
 
-        self._add_ins("invokevirtual java/io/PrintStream/println(%s)V" % var_type)
+        self._add_ins("invokevirtual java/io/PrintStream/println(%s)V" % self.print_var_type_table[var_type])
 
 
     def enterProgram(self, ctx):
@@ -81,8 +114,8 @@ class pccPrintListener(pccListener):
 
 
     def enterDeclaration(self, ctx):
-        vartype, ids, _ = list(ctx.getChildren())
-        vartype = vartype.getText()
+        var_type, ids, _ = list(ctx.getChildren())
+        var_type = var_type.getText()
 
         for child in ids.getChildren():
             if child.getText() == ',':
@@ -92,16 +125,15 @@ class pccPrintListener(pccListener):
             c_count = c.getChildCount()
 
             if c_count == 0 or c_count == 3: # var
-                var_id = self.newVar((c.getChild(0) if c_count == 3 else c).getText()) # TODO: raise exception if variable exists
-                if vartype == 'int':
-                    self._add_ins("bipush 0")
-                    self._add_ins("istore %s" % var_id)
+                var_id = self.newVar((c.getChild(0) if c_count == 3 else c).getText(), var_type) # TODO: raise exception if variable exists
+                self._push_constant(0, var_type)
+                self._add_ins("store %s" % var_id, var_type)
 
             elif c_count == 4: # array
-                var_id = self.newVar(c.getChild(0).getText()) # TODO: raise exception if variable exists
+                var_id = self.newVar(c.getChild(0).getText(), var_type) # TODO: raise exception if variable exists
                 size = c.getChild(2).getText()
-                self._add_ins("bipush %s" % size)
-                self._add_ins("newarray %s" % vartype)
+                self._add_ins("ldc %s" % size)
+                self._add_ins("newarray %s" % var_type)
                 self._add_ins("astore %s" % var_id)
 
             if c_count == 3: # declration with assignment
@@ -113,56 +145,60 @@ class pccPrintListener(pccListener):
             assignee, _, value = list(ctx.getChildren())
             assignee = self.getLastChild(assignee)
             if assignee.getChildCount() == 0: # identifier
-                var_id = self.getVar(assignee.getText(), self.block_number)
-                self.calculateExpression(value)
-                self._add_ins("istore %s" % var_id)
+                var_id, var_type = self.getVar(assignee.getText(), self.block_number)
+                self.calculateExpression(value, var_type)
+                self._add_ins("store %s" % var_id, var_type)
 
             elif assignee.getChildCount() == 4: # array
-                var_id = self.getVar(assignee.getChild(0).getText(), self.block_number)
+                var_id, var_type = self.getVar(assignee.getChild(0).getText(), self.block_number)
                 index = assignee.getChild(2)
                 self._add_ins('aload %s' % var_id)
-                self.calculateExpression(index)
-                self.calculateExpression(value)
-                self._add_ins('iastore')
+                self.calculateExpression(index, 'int')
+                self.calculateExpression(value, var_type)
+                self._add_ins('astore', var_type)
 
             elif assignee.getChildCount() == 3: # struct
                 pass
 
 
-    def calculateExpression(self, ctx):
+    def calculateExpression(self, ctx, var_type):
         ctx = self.getLastChild(ctx)
 
         if ctx.getChildCount() == 0: # identifier or constant value
-            if ctx.getSymbol().type == pccLexer.Identifier:
-                var_id = self.getVar(ctx.getText(), self.block_number)
-                self._add_ins("iload %s" % var_id)
-            else:
-                self._add_ins("bipush %s" % ctx.getText())
+            if ctx.getSymbol().type == pccLexer.Identifier: # identifier
+                var_id, vt = self.getVar(ctx.getText(), self.block_number)
+                if vt != var_type:
+                    raise TypeError("wrong type '%s'" % ctx.getText())
+                self._add_ins("load %s" % var_id, var_type)
+            else: # constant value
+                self._push_constant(ctx.getText(), var_type)
 
         if ctx.getChildCount() == 4: # array value
-            var_id = self.getVar(ctx.getChild(0).getText(), self.block_number)
+            var_id, vt = self.getVar(ctx.getChild(0).getText(), self.block_number)
+            if vt != var_type:
+                raise TypeError("wrong type '%s'" % ctx.getText())
             arr_index = ctx.getChild(2)
             self._add_ins('aload %s' % var_id)
-            self.calculateExpression(arr_index)
-            self._add_ins('iaload')
+            self.calculateExpression(arr_index, 'int')
+            self._add_ins('aload', var_type)
 
         elif ctx.getChildCount() == 3: # expr
             if ctx.getChild(0).getText() == '(':
-                self.calculateExpression(ctx.getChild(1))
+                self.calculateExpression(ctx.getChild(1), var_type)
             else:
                 i1, o, i2 = ctx.getChildren()
                 o = o.getText()
 
-                self.calculateExpression(i1)
-                self.calculateExpression(i2)
+                self.calculateExpression(i1, var_type)
+                self.calculateExpression(i2, var_type)
                 if o == '+':
-                    self._add_ins("iadd")
+                    self._add_ins("add", var_type)
                 elif o == '-':
-                    self._add_ins("isub")
+                    self._add_ins("sub", var_type)
                 elif o == '*':
-                    self._add_ins("imul")
+                    self._add_ins("mul", var_type)
                 elif o == '/':
-                    self._add_ins("idiv")
+                    self._add_ins("div", var_type)
 
 
 
